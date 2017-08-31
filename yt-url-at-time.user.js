3867e2900eb679c67c7e1147d170e8467104894b
MechaLynx <mechalynx88@gmail.com>
2017-08-31 10:00:03 +0300

    Add missing semicolons

--- yt-url-at-time.user.js
+++ yt-url-at-time.user.js
@@ -11,23 +11,24 @@
 
 var re_timehash = /#t=([0-9]*(h|m|s))*/g;
 var video = {
-  element: document.getElementsByTagName('video') [0],
-  get timehash() {
-    var secs = this.element.currentTime;
-    return '#t=' + [(h = ~~(secs / 3600)) && h + 'h' || null,
-    (m = ~~(secs % 3600 / 60)) && m + 'm' || null,
-    (s = ~~(secs % 3600 % 60)) && s + 's'].join('');
-  },
-  get notimehash() {
-    return window.location.origin +
-    window.location.pathname +
-    window.location.search +
-    window.location.hash.replace(re_timehash, '');
-  }
-}
+    element: document.getElementsByTagName('video')[0],
+    get timehash() {
+        var secs = this.element.currentTime;
+        return '#t=' + [(h = ~~(secs / 3600)) && h + 'h' || null,
+            (m = ~~(secs % 3600 / 60)) && m + 'm' || null,
+            (s = ~~(secs % 3600 % 60)) && s + 's'
+        ].join('');
+    },
+    get notimehash() {
+        return window.location.origin +
+            window.location.pathname +
+            window.location.search +
+            window.location.hash.replace(re_timehash, '');
+    }
+};
 document.addEventListener('keydown', z => {
-  // if you want to change the hotkey
-  // you can use this: http://mechalynx.github.io/keypress/
-  // or another tester if you don't like this one
-  z.altKey && 'Backquote' === z.code && history.replaceState(false, false, video.notimehash + video.timehash)
+    // if you want to change the hotkey
+    // you can use this: http://mechalynx.github.io/keypress/
+    // or another tester if you don't like this one
+    z.altKey && 'Backquote' === z.code && history.replaceState(false, false, video.notimehash + video.timehash);
 });

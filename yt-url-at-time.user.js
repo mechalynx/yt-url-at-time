f31a247014a72a96ddfd3c4dad0f787ff013eb69
MechaLynx <mechalynx88@gmail.com>
2017-11-05 09:29:50 +0200

    Fix script loading point

--- yt-url-at-time.user.js
+++ yt-url-at-time.user.js
@@ -4,31 +4,37 @@
 // @grant       none
 // @description On youtube, use alt+` to set the url to the current timestamp, for easy bookmarking
 // @include     https://www.youtube.tld/watch*
-// @version     0.0.9
+// @version     0.0.10
 // @copyright   2017, MechaLynx (https://github.com/MechaLynx)
 // @updateURL   https://openuserjs.org/meta/MechaLynx/yt-url-at-time.meta.js
+// @run-at document-idle
 // ==/UserScript==
 
 var re_timehash = /#t=([0-9]*(h|m|s))*/g;
+
 var video = {
-    element: document.getElementsByTagName('video')[0],
-    get timehash() {
-        var secs = this.element.currentTime;
-        return '#t=' + [(h = ~~(secs / 3600)) && h + 'h' || null,
-            (m = ~~(secs % 3600 / 60)) && m + 'm' || null,
-            (s = ~~(secs % 3600 % 60)) && s + 's'
-        ].join('');
-    },
-    get notimehash() {
-        return window.location.origin +
-            window.location.pathname +
-            window.location.search +
-            window.location.hash.replace(re_timehash, '');
-    }
+  element: document.getElementsByTagName('video') [0],
+  get timehash() {
+    var secs = this.element.currentTime;
+    return '#t=' + [(h = ~~(secs / 3600)) && h + 'h' || null,
+    (m = ~~(secs % 3600 / 60)) && m + 'm' || null,
+    (s = ~~(secs % 3600 % 60)) && s + 's'].join('');
+  },
+  get notimehash() {
+    return window.location.origin +
+    window.location.pathname +
+    window.location.search +
+    window.location.hash.replace(re_timehash, '');
+  }
 };
+
+var waiter = window.addEventListener('load', function(e){
+  video.element = document.getElementsByTagName('video') [0]
+});
+
 document.addEventListener('keydown', z => {
-    // if you want to change the hotkey
-    // you can use this: http://mechalynx.github.io/keypress/
-    // or another tester if you don't like this one
-    z.altKey && 'Backquote' === z.code && history.replaceState(false, false, video.notimehash + video.timehash);
+  // if you want to change the hotkey
+  // you can use this: http://mechalynx.github.io/keypress/
+  // or another tester if you don't like this one
+  z.altKey && 'Backquote' === z.code && history.replaceState(false, false, video.notimehash + video.timehash);
 });

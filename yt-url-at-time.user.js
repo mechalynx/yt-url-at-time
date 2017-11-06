d875a4b2894ee544b12ca3ff817b0b19b330dc22
MechaLynx <mechalynx88@gmail.com>
2017-11-06 07:35:03 +0200

    Disable hover style during ads

--- yt-url-at-time.user.js
+++ yt-url-at-time.user.js
@@ -4,11 +4,12 @@
 // @grant       none
 // @description On youtube, use alt+` to set the url to the current timestamp, for easy bookmarking
 // @include     https://www.youtube.tld/*
-// @version     0.1.1
+// @version     0.1.2
 // @copyright   2017, MechaLynx (https://github.com/MechaLynx)
 // @updateURL   https://openuserjs.org/meta/MechaLynx/yt-url-at-time.meta.js
 // @run-at document-idle
 // ==/UserScript==
+// jshint esversion: 6
 
 var re_timehash = /#t=([0-9]*(h|m|s))*/g;
 
@@ -34,8 +35,24 @@ var wait_for_page = window.setInterval(function(){
   var current_time_element = document.querySelector('.ytp-time-current');
   if (current_time_element){
     window.clearInterval(wait_for_page);
+
+    let time_style = document.createElement('style');
+    time_style.innerHTML = `
+      .url-at-time-element-hover:hover{
+        cursor: pointer;
+      }
+	`;
+    document.body.appendChild(time_style);
+
     console.log(current_time_element);
-    current_time_element.setAttribute('style', current_time_element.getAttribute('style') ? current_time_element.getAttribute('style') + "cursor: pointer;" : "cursor: pointer;");
+    current_time_element.onmouseover = function(){
+      if (document.querySelector('.videoAdUi')){
+        current_time_element.classList.remove('url-at-time-element-hover');
+      }else{
+        current_time_element.classList.add('url-at-time-element-hover');
+      }
+    };
+
     current_time_element.addEventListener('click', hashmodifier);
   }
 }, 1000);
@@ -44,7 +61,7 @@ var hashmodifier = function(){
   if ( location.href.match(/.*watch.*/) && document.querySelector('.videoAdUi') === null){
     history.replaceState(false, false, video.notimehash + video.timehash);
   }
-}
+};
 
 document.addEventListener('keydown', z => {
   // if you want to change the hotkey
